19c04b5820a482d4d28f7ece4d507928878cbf86
kabeer-necrox <khushkabeer@gmail.com>
2024-08-22 12:15:19 -0400

    here is the pand code

---
+++ first.py
@@ -0,0 +1,77 @@
+# print("hello israr")
+
+# name = "israr"
+# print(name)
+
+# num1 = 10
+# num2 = 20
+# num3 = num1 * num2
+# print(num3)
+
+
+# name = "i am computer science student i am passionate about programming
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ second.py
@@ -0,0 +1,78 @@
+
+
+
+# this is my first pyton prjrogam
+
+# import sys
+
+# print(sys.version)
+
+
+# if 5 > 2:
+#  print("Five is greater than two!")
+
+
+
+# if 5 > 2:
+#  print("Five is greater than two!") 
+# if 5 > 2:
+#         print("Five is greater than two!") 
+
+
+
+
+# name = "israr"
+# print(name)
+
+
+
+
+
+# if 5 > 2:
+#   print("Five is greater than two!")
+#  print("Five is greater than two!")
+
+
+
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# x = "cat"
+# print(x)
+
+
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+
+
+
+
+import pandas as pd
+
+print(pd.__version__)
+
+
+# import pandas as pd
+
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2]
+# }
+
+# myvar = pd.DataFrame(mydataset)
+
+# print(myvar)
+
+
+
+
+
+
+
+
+
+
+
